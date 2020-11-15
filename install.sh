dcecce9510a4379f8ac4aaf8552676b534cf097d
3n3a <46775561+3n3a@users.noreply.github.com>
2020-11-15 23:03:43 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 path=/home/$USER/analyze_pp
 url_pandoc=https://github.com/jgm/pandoc/releases/download/2.11.1.1/pandoc-2.11.1.1-1-amd64.deb
-url_python_file=file...
-url_python_req=file...
+url_python_file=https://raw.githubusercontent.com/3n3a/eneas_pluspoints_parser/main/analyze.py
+url_python_req=https://raw.githubusercontent.com/3n3a/eneas_pluspoints_parser/main/requirements.txt
 
 apt install -y wget python3 python3-pip python3-venv
 
