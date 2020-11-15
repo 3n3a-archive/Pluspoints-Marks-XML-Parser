67162db3a07b003e44cbca031a03ee2d5f85e5b6
3n3a <46775561+3n3a@users.noreply.github.com>
2020-11-15 23:03:00 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+path=/home/$USER/analyze_pp
+url_pandoc=https://github.com/jgm/pandoc/releases/download/2.11.1.1/pandoc-2.11.1.1-1-amd64.deb
+url_python_file=file...
+url_python_req=file...
+
+apt install -y wget python3 python3-pip python3-venv
+
+mkdir -p $path
+cd $path
+
+wget $url_pandoc
+wget $url_python_file
+wget $url_python_req
+
+dpkg -i pandoc-*.deb
+rm -rf pandoc-*.deb
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip install -r requirements.txt
+echo "installed everything...hopefully.\nexecute analyzer.py [file] - have fun"
